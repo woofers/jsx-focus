058f782c15dadada76d5dbd9492c1502270eb478
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-02-21 07:24:13 +0000

    Fix namespace conflict

--- packages/jsx-focus/lib/index.mjs
+++ packages/jsx-focus/lib/index.mjs
@@ -1,18 +1,18 @@
 import {
-  enableBodyScroll, 
-  disableBodyScroll,
-  Portal,
-  TrapFocus,
-  canUseDOM,
-  hasWindow,
-  isIosDevice,
-  setRef,
-  getOwnerDocument,
-  useAriaHidden,
-  useForceUpdate,
-  useForkRef,
-  useLayoutEffect,
-  useScrollLock
+  enableBodyScroll as enableBodyScrollProd, 
+  disableBodyScroll as disableBodyScrollProd,
+  Portal as PortalProd,
+  TrapFocus as TrapFocusProd,
+  canUseDOM as canUseDOMProd,
+  hasWindow as hasWindowProd,
+  isIosProdice as isIosProdiceProd,
+  setRef as setRefProd,
+  getOwnerDocument as getOwnerDocumentProd,
+  useAriaHidden as useAriaHiddenProd,
+  useForceUpdate as useForceUpdateProd,
+  useForkRef as useForkRefProd,
+  useLayoutEffect as useLayoutEffectProd,
+  useScrollLock as useScrollLockProd
 } from './jsx-focus.mjs'
 import {
   enableBodyScroll as enableBodyScrollDev, 
@@ -31,17 +31,17 @@ import {
   useScrollLock as useScrollLockDev
 } from './jsx-focus.dev.mjs'
 
-export const enableBodyScroll = process.env.NODE_ENV !== 'production' ? enableBodyScrollDev : enableBodyScroll
-export const disableBodyScroll = process.env.NODE_ENV !== 'production' ? disableBodyScrollDev : disableBodyScroll
-export const Portal = process.env.NODE_ENV !== 'production' ? PortalDev : Portal
-export const TrapFocus = process.env.NODE_ENV !== 'production' ? TrapFocusDev : TrapFocus
-export const canUseDOM = process.env.NODE_ENV !== 'production' ? canUseDOMDev : canUseDOM
-export const hasWindow = process.env.NODE_ENV !== 'production' ? hasWindowDev : hasWindow
-export const isIosDevice = process.env.NODE_ENV !== 'production' ? isIosDeviceDev : isIosDevice
-export const setRef = process.env.NODE_ENV !== 'production' ? setRefDev : setRef
-export const getOwnerDocument = process.env.NODE_ENV !== 'production' ? getOwnerDocumentDev : getOwnerDocument
-export const useAriaHidden = process.env.NODE_ENV !== 'production' ? useAriaHiddenDev : useAriaHidden
-export const useForceUpdate = process.env.NODE_ENV !== 'production' ? useForceUpdateDev : useForceUpdate
-export const useForkRef = process.env.NODE_ENV !== 'production' ? useForkRefDev : useForkRef
-export const useLayoutEffect = process.env.NODE_ENV !== 'production' ? useLayoutEffectDev : useLayoutEffect
-export const useScrollLock = process.env.NODE_ENV !== 'production' ? useScrollLockDev : useScrollLock
+export const enableBodyScroll = process.env.NODE_ENV !== 'production' ? enableBodyScrollDev : enableBodyScrollProd
+export const disableBodyScroll = process.env.NODE_ENV !== 'production' ? disableBodyScrollDev : disableBodyScrollProd
+export const Portal = process.env.NODE_ENV !== 'production' ? PortalDev : PortalProd
+export const TrapFocus = process.env.NODE_ENV !== 'production' ? TrapFocusDev : TrapFocusProd
+export const canUseDOM = process.env.NODE_ENV !== 'production' ? canUseDOMDev : canUseDOMProd
+export const hasWindow = process.env.NODE_ENV !== 'production' ? hasWindowDev : hasWindowProd
+export const isIosDevice = process.env.NODE_ENV !== 'production' ? isIosDeviceDev : isIosProdiceProd
+export const setRef = process.env.NODE_ENV !== 'production' ? setRefDev : setRefProd
+export const getOwnerDocument = process.env.NODE_ENV !== 'production' ? getOwnerDocumentDev : getOwnerDocumentProd
+export const useAriaHidden = process.env.NODE_ENV !== 'production' ? useAriaHiddenDev : useAriaHiddenProd
+export const useForceUpdate = process.env.NODE_ENV !== 'production' ? useForceUpdateDev : useForceUpdateProd
+export const useForkRef = process.env.NODE_ENV !== 'production' ? useForkRefDev : useForkRefProd
+export const useLayoutEffect = process.env.NODE_ENV !== 'production' ? useLayoutEffectDev : useLayoutEffectProd
+export const useScrollLock = process.env.NODE_ENV !== 'production' ? useScrollLockDev : useScrollLockProd
